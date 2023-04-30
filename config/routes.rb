47daf6fad994c32cacad6380db041ed4d3e32092
riffiy <riffiy.6135@gmail.com>
2023-04-30 11:32:51 +0000

    rails_tutorial_8.4.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -6,4 +7,7 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
