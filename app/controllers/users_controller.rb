6a585fa81a47f35024ca00f5415efc32c0388a2f
riffiy <riffiy.6135@gmail.com>
2023-05-04 12:18:28 +0000

    rails _tutorial10.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to @user
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
